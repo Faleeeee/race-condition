ea3784bbaa11f3f7d3ba51d26911fd21f98b7ec4
Faleeeee <letranthuanphat111@gmail.com>
2024-10-29 09:31:09 +0700

    update time ticket

--- src/Models/Ticket.js
+++ src/Models/Ticket.js
@@ -1,9 +1,9 @@
 const db = require('../config/db');
 
 const Ticket = {
-    bookTicket: (match_id, user_id, seat_number, bookingDate, callback) => {
-        const sql = 'INSERT INTO tickets (match_id, user_id, seat_number, booking_date) VALUES (?, ?, ?, ?)';
-        const values = [match_id, user_id, seat_number, bookingDate];
+    bookTicket: (match_id, user_id, seat_number, callback) => {
+        const sql = 'INSERT INTO tickets (match_id, user_id, seat_number, booking_time) VALUES (?, ?, ?, NOW())';
+        const values = [match_id, user_id, seat_number];
 
         db.query(sql, values, (err, result) => {
             if (err) {
